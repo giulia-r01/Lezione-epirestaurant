63cf2fc94c0b1a6d9677eacac5939d25bf546ee9
Giulia Rizzo <girzzo@gmail.com>
2025-04-01 12:08:29 +0200

    add menu

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col, Carousel } from "react-bootstrap"
+import pastasciutte from "../data/menu.json"
 
 const Home = function () {
   return (
@@ -7,6 +8,27 @@ const Home = function () {
         <Col xs={12} md={8} lg={6} className="text-center">
           <h1>Epistaurant</h1>
           <h2>Le migliori pastasciutte del web</h2>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={6} className="text-center">
+          <Carousel>
+            {pastasciutte.map((pasta) => {
+              return (
+                <Carousel.Item key={pasta.id}>
+                  <img
+                    src={pasta.image}
+                    className="w-100"
+                    alt={"immagine di " + pasta.name}
+                  />
+                  <Carousel.Caption>
+                    <h3>{pasta.name}</h3>
+                    <p>{pasta.description}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              )
+            })}
+          </Carousel>
         </Col>
       </Row>
     </Container>
