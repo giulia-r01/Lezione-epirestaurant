ad1a77af9221503abd97f389ab1a5634b9f1b186
Giulia Rizzo <girzzo@gmail.com>
2025-04-02 13:01:42 +0200

    lezione-2-04-epistaurant

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import CustomNavbar from "./components/CustomNavbar"
 import Home from "./components/Home"
+import ReservationForm from "./components/ReservationForm"
 
 function App() {
   return (
     <main>
       <CustomNavbar tema="dark" />
+      <ReservationForm />
       <Home />
     </main>
   )
---
+++ src/components/ReservationForm.jsx
@@ -0,0 +1,217 @@
+import { Component } from "react"
+import { Container, Row, Col, Alert } from "react-bootstrap"
+import Button from "react-bootstrap/Button"
+import Form from "react-bootstrap/Form"
+
+// Il backender mi da l'oggetto da mandare alle api, che deve contenere
+// name
+// phone
+// numberOfPeople
+// smoking
+//dateTime
+//specialRequest
+
+// ci serve uno stato per memorizzare i dati del form che gli utenti hanno compilato
+class ReservationForm extends Component {
+  state = {
+    reservation: {
+      name: "",
+      phone: "",
+      numberOfPeople: "1",
+      smoking: "",
+      dateTime: false,
+      specialRequest: "",
+    },
+  }
+
+  // ora dobbiamo collegare i valori delle proprietà inserite nello stato 'reservation
+  // gli INPUT CONTROLLATI sono gestiti da un 'two-way-databinding' (a doppia mandata) perché i campi sono collegati sia in lettura che in scrittura
+  // lo stato determina cosa l'imput avrà come value (stringa vuota nel nostro caso)
+  // ma se noi scriviamo sull'imput aggiorniamo lo stato digitando lettera per lettera, quindi
+  // settiamo lo stato, e ciò che viene settato si vedrà nel campo dell'input
+  //per svuotare il form andrà 'svuotato' lo stato
+
+  // logica del submit del form
+  handleSubmit = (e) => {
+    //usiamo le funzioni freccia per evitare un uso scorretto di this
+    e.preventDefault()
+    console.log("funziona, tra poco faccio la fetch")
+    fetch("https://striveschool-api.com/api/reservation", {
+      method: "POST",
+      body: JSON.stringify(this.state.reservation),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert("Prenotazione salvata")
+          //resetto il form ripristinando lo state alla forma iniziale
+          this.setState({
+            reservation: {
+              name: "",
+              phone: "",
+              numberOfPeople: "1",
+              smoking: "",
+              dateTime: false,
+              specialRequest: "",
+            },
+          })
+        } else {
+          throw new Error("La chiamata non ha restituito esito positivo")
+        }
+      })
+      .catch((error) => {
+        console.log("si è verificato un errore", error)
+      })
+  }
+
+  render() {
+    return (
+      <Container>
+        <Form onSubmit={this.handleSubmit}>
+          {/* ho collegato al submit del form*/}
+          <Row className="justify-content-center">
+            <Col xs={12} md={8} lg={6}>
+              <Form.Group className="mb-3">
+                <Form.Label>Il tuo nome</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Mario Rossi"
+                  required
+                  //colleghiamo il campo nome allo stato del componente
+                  //collego il valore dell'input name allo stato
+                  value={this.state.reservation.name}
+                  // collego la modifica dell'input alla modifica dello stato
+                  onChange={(e) => {
+                    this.setState({
+                      reservation: {
+                        ...this.state.reservation, // fa in modo che riparta dall'attuale contenuto dello state (quindi non perderò gli step precedenti)
+                        name: e.target.value, // aggiungo l'attuale contenuto
+                      },
+                    })
+                  }}
+                />
+              </Form.Group>
+
+              {/*RENDERING CONDIZIONALE*/}
+              {/*Metodo dello 'SHORT CIRCUIT'*/}
+              {this.state.reservation.name === "Al Bano" && (
+                <Alert variant="info">
+                  Un bicchiere di vino con un panino!
+                </Alert>
+              )}
+
+              <Form.Group className="mb-3">
+                <Form.Label>N. di telefono</Form.Label>
+                <Form.Control
+                  type="tel"
+                  required
+                  value={this.state.reservation.phone}
+                  // collego la modifica dell'input alla modifica dello stato
+                  onChange={(e) => {
+                    this.setState({
+                      reservation: {
+                        ...this.state.reservation,
+                        phone: e.target.value,
+                      },
+                    })
+                  }}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Quanti siete?</Form.Label>
+                <Form.Select
+                  aria-label="Numero di persone"
+                  //collego il valore dell'input name allo stato
+                  value={this.state.reservation.numberOfPeople}
+                  // collego la modifica dell'input alla modifica dello stato
+                  onChange={(e) => {
+                    this.setState({
+                      reservation: {
+                        ...this.state.reservation,
+                        numberOfPeople: e.target.value,
+                      },
+                    })
+                  }}
+                >
+                  <option>Open this select menu</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                  <option>7</option>
+                  <option>8</option>
+                </Form.Select>
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Quando venite?</Form.Label>
+                <Form.Control
+                  type="datetime-local"
+                  required
+                  value={this.state.reservation.dateTime}
+                  // collego la modifica dell'input alla modifica dello stato
+                  onChange={(e) => {
+                    this.setState({
+                      reservation: {
+                        ...this.state.reservation,
+                        dateTime: e.target.value,
+                      },
+                    })
+                  }}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Check
+                  type="checkbox"
+                  label="Tavolo fumatori?"
+                  checked={this.state.reservation.smoking}
+                  // collego la modifica dell'input alla modifica dello stato
+                  // la proprietà value dei campi checkbox non torna true/false ma on/off (come stringa)
+                  // poiché le API si aspettano un valore boolenao per la proprietà smoking devo usare checked anziché value
+                  onChange={(e) => {
+                    this.setState({
+                      reservation: {
+                        ...this.state.reservation,
+                        smoking: e.target.checked,
+                      },
+                    })
+                  }}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Allergie, intolleranze?</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  required
+                  value={this.state.reservation.specialRequest}
+                  // collego la modifica dell'input alla modifica dello stato
+                  onChange={(e) => {
+                    this.setState({
+                      reservation: {
+                        ...this.state.reservation,
+                        specialRequest: e.target.value,
+                      },
+                    })
+                  }}
+                />
+              </Form.Group>
+              <Button variant="success" type="submit">
+                Prenota
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    )
+  }
+}
+
+export default ReservationForm
