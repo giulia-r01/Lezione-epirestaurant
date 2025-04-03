d16c3ccd97058831fb4200e3c429ff6e6b0637fb
Giulia Rizzo <girzzo@gmail.com>
2025-04-03 10:59:44 +0200

    Update Backoffice.jsx

--- src/components/Backoffice.jsx
+++ src/components/Backoffice.jsx
@@ -10,7 +10,53 @@ class Backoffice extends Component {
     reservations: [], // ospiterà l'elenco delle prenotazioni che arriveranno dalla chiamata GET
   }
 
+  getReservations = () => {
+    //questa funzione dovrà recuperare le prenotazioni a db tramite una fetch GET e salvarle all'interno di reservations nello state
+
+    const URL = "https://striveschool-api.herokuapp.com/api/reservation"
+    fetch(URL)
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error("errore nella fetch")
+        }
+      })
+      .then((data) => {
+        console.log("Prenotazioni", data)
+        this.setState({
+          reservations: data,
+        })
+      })
+      .catch((err) => {
+        console.log("errore", err)
+      })
+  }
+
+  //un altro metodo di lifecyrcle
+
+  componentDidMount = () => {
+    //metodo riservato per i componenti react a classe
+    // componentDiMount viene invocato UNA VOLTA SOLA per lifecycle
+    // viene eseguito subito dopo la prima invocazione di render()
+    this.getReservations()
+  }
+
+  // STEP DEL CICLO DI VITA DI BACKOFFICE.JSX
+  //1) viene inizializzato lo stato del componente, quindi reservations è []
+  //2) viene invocato render() per la prima volta, che disegna la parte statica del componente
+  //3) se c'è, viene invocato componentDiMount(), che fa la GET + setState()
+  //4) a causa di quel setState, render() viene invocato
+  //5) la lista viene riempita con i dati appena messi nello stato
+
   render() {
+    // this.getReservations() // provo ad invocare getReservation nel render (metodo che si occupa
+    // di disegnare l'interfaccia)
+
+    //render() viene automaticamente invocato dal componente a classe, è un metodo di 'lifecycle'
+    // viene invocato automaticamente all'avvio della pagina, ma react RE-INVOCA render()
+    // ogni volta che cambia lo STATE del componente o le PROPS
+    //mai mettere un setState dentro un render!!
     return (
       <Container>
         <Row className="justify-content-center my-5">
